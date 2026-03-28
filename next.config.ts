5501f9ce5afb28bccfdb388e1f0feffbd719b0c4
Ye Pyae Aung <yepyaeaung1311999@gmail.com>
2026-03-28 13:36:24 +0630

    YP: update

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: false, // ✅ reduce extra render checks
+
+  output: "standalone", // ✅ best for Docker (VERY IMPORTANT)
+  
+  typescript: {
+    ignoreBuildErrors: true, // ⚠️ optional (skip type check)
+  },
+
+  experimental: {
+    workerThreads: false, // ✅ reduce CPU spikes
+    cpus: 1, // ✅ limit parallelism (VERY IMPORTANT for small VPS)
+  },
 };
 
 export default nextConfig;
